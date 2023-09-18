a580e577997cd8e883491056cd42f9e2d86d00f7
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2023-09-18 10:51:01 +0100

    intial commit

--- index.js
+++ index.js
@@ -1,90 +1,45 @@
-
 class PhotoGallery {
-    constructor(){
-        this.API_KEY = '563492ad6f91700001000001de0061331c5c4b77afac0e87110a6042';
+    constructor() {
         this.galleryDiv = document.querySelector('.gallery');
         this.searchForm = document.querySelector('.header form');
         this.loadMore = document.querySelector('.load-more');
         this.logo = document.querySelector('.logo');
-        this.pageIndex = 1;
-        this.searchValueGlobal = '';
-        this.eventHandler();
+        this.imageFilenames = ['img/photo1.jpg', 'img/photo2.jpg', 'img/photo3.jpg', 'img/photo4.jpg', 'img/photo5.jpg', 'img/photo6.jpg', 'img/photo7.jpg', 'img/photo8.jpg', 'img/photo9.jpg', 'img/photo10.jpg', 'img/photo11.jpg', 'img/photo12.jpg', 'img/photo13.jpg', 'img/photo14.jpg', 'img/photo15.jpg'];
+
+        this.GenerateHTML(this.imageFilenames); // Load images once the page is initialized
     }
-    eventHandler(){
-        document.addEventListener('DOMContentLoaded', ()=> {
-            this.getImg(1);
-        });
-        this.searchForm.addEventListener('submit', (e)=>{
-            this.pageIndex = 1;
-            this.getSearchedImages(e);
-        });
-        this.loadMore.addEventListener('click', (e)=>{
-            this.loadMoreImages(e);
-        })
-        this.logo.addEventListener('click', ()=>{
-            this.pageIndex = 1;
-            this.galleryDiv.innerHTML = '';
-            this.getImg(this.pageIndex);
-        })
-    }
-    async getImg(index){
-        this.loadMore.setAttribute('data-img', 'curated')
-        const baseURL = `https://api.pexels.com/v1/curated?page=${index}&per_page=12`;
-        const data = await this.fetchImages(baseURL);
-        this.GenerateHTML(data.photos)
-        
-    }
-    async fetchImages(baseURL){
-        const response = await fetch(baseURL, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                Authorization: this.API_KEY
-            }
-        });
-        const data = await response.json();
-        return data;
-    }
-    GenerateHTML(photos){
-        photos.forEach(photo => {
+
+    GenerateHTML(imageFilenames) {
+        imageFilenames.forEach((filename, index) => {
             const item = document.createElement('div');
             item.classList.add('item');
+
+            const imageNumber = index + 1;
+
             item.innerHTML = `
-            <a href='${photo.src.original}' target='_blank'>
-                <img src="${photo.src.medium}">
-                <h3>${photo.photographer}</h3>
-            </a>
+                <a href="${filename}" target="_blank">
+                    <img src="${filename}" alt="Image ${imageNumber}">
+                    <h3>Image ${imageNumber}</h3>
+                </a>
             `;
-            this.galleryDiv.appendChild(item)
-        })
-    }
-    async getSearchedImages(e){
-        this.loadMore.setAttribute('data-img', 'search')
-        e.preventDefault();
-        this.galleryDiv.innerHTML = '';
-        const searchValue = e.target.querySelector('input').value;
-        this.searchValueGlobal = searchValue;
-        const baseURL = `https://api.pexels.com/v1/search?query=${searchValue}&page=1&per_page=12`
-        const data = await this.fetchImages(baseURL);
-        this.GenerateHTML(data.photos);
-        e.target.reset();
-    }
-    async getMoreSearchedImages(index){
-        const baseURL = `https://api.pexels.com/v1/search?query=${this.searchValueGlobal}&page=${index}&per_page=12`
-        const data = await this.fetchImages(baseURL);
-        this.GenerateHTML(data.photos);
-    }
-    loadMoreImages(e){
-        let index = ++this.pageIndex;
-        const loadMoreData = e.target.getAttribute('data-img');
-        if(loadMoreData === 'curated') {
-            //load page 2 for curated
-            this.getImg(index)
-        } else {
-            //load page 2 for search
-            this.getMoreSearchedImages(index);
-        }
+            this.galleryDiv.appendChild(item);
+        });
     }
 }
 
-const gallery = new PhotoGallery;
+// Initialize the PhotoGallery class when the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    new PhotoGallery();
+});
+
+const openPopupButton = document.getElementById('openPopup');
+const closePopupButton = document.getElementById('closePopup');
+const popup = document.getElementById('popup');
+
+openPopupButton.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+closePopupButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
